4e09e1806c6631a10cc729649474f18079ae6c4d
Cristian Garcia <cgarcia.e88@gmail.com>
2018-02-02 16:44:43 -0800

    load images + split + batch generators

--- dataget/__init__.py
+++ dataget/__init__.py
@@ -13,8 +13,11 @@ from .version import __version__  # NOQA
 from . import utils
 from . import dataset
 from .api import ls, data, get_path, DATASETS
-from .dataset_loader import load_custom_datasets, load_plugin_datasets, load_local_datasets
-
+from .dataset_loader import (
+    load_custom_datasets, load_plugin_datasets, load_local_datasets)
+from .functions import (
+    load_images, split, batch_generator, epochs_batch_generator, 
+    infinite_random_batch_generator )
 
 load_custom_datasets(DATASETS)
 load_plugin_datasets(DATASETS)
