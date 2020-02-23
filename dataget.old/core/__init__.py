d985e42b55a92e17a228c8d07262dbf6c717845a
Cristian Garcia <cgarcia.e88@gmail.com>
2020-02-23 14:15:33 -0500

    remove dataget.old

--- dataget.old/core/__init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import print_function, absolute_import, unicode_literals, division
-from .dataset import DataSet 
-from .image_dataset import ImageDataSet
-from .image_navigation import ImageNavigationDataSet
