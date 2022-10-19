37da7af919924deb0d7e2880015e353a7652739a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-19 02:50:02 +0000

    Adding tarfile member sanitization to extractall()

--- dataget/image/cifar.py
+++ dataget/image/cifar.py
@@ -31,7 +31,26 @@ class cifar10(Dataset):
             )
 
         with tarfile.open(gz_path) as zip_ref:
-            zip_ref.extractall(self.path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(zip_ref, self.path)
 
         gz_path.unlink()
 
@@ -76,7 +95,26 @@ class cifar100(Dataset):
             )
 
         with tarfile.open(gz_path) as zip_ref:
-            zip_ref.extractall(self.path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(zip_ref, self.path)
 
         gz_path.unlink()
 
